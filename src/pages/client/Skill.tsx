7bf48d628327f13f24bfdcdec011a2884c7d6cfb
Ptran <106719083+Migu03@users.noreply.github.com>
2023-07-14 00:11:06 +0700

    update skill

--- src/pages/client/Skill.tsx
+++ src/pages/client/Skill.tsx
@@ -13,17 +13,14 @@ const Skill = ({ skill }: Props) => {
         These are the tools, languages, and technologies that I use to make my product everyday
       </p>
       <div className="section-skills">
-        <div className="section-skills-item"> 
           {skill?.map((item: ITechnology) => {
 
             return (
               <div className="skills__data" key={item._id}>
                 <i className={`bx bxl-${item.tag}`}></i>
-                <h3 className="skills__name">{item.name}</h3>
               </div>
             )
           })}
-        </div>
       </div>
     </div>
   )
